6bd24d9f04489250b4728ed383fb7d8cb87f526f
Chang Xian Xiang <changxianxiang@gmail.com>
2024-08-25 04:04:35 +0800

    update form styling

--- food-match/src/pages/profile.tsx
+++ food-match/src/pages/profile.tsx
@@ -13,6 +13,7 @@ import { updateMeSchema } from '~/schemas/me'
 import { trpc } from '~/utils/trpc'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
+import { Navbar } from '~/features/common/components'
 
 export const Profile = () => {
   const { me } = useMe()
@@ -48,77 +49,102 @@ export const Profile = () => {
     return updateMeMutation.mutate(input)
   }, console.error)
   return (
-    <Stack spacing="1rem" alignItems="center" justifyContent="center">
-      <FormControl
-        id="name"
-        isRequired
-        isInvalid={!!errors.name}
-        isReadOnly={updateMeMutation.isLoading}
+    <>
+      <Navbar />
+      <Stack
+        spacing="1rem"
+        alignItems="center"
+        justifyContent="center"
+        pt="1rem"
       >
-        <FormLabel>Name</FormLabel>
-        <Input {...register('name')} placeholder="Name" borderRadius={'15px'} />
-        <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
-      </FormControl>
-      <FormControl
-        id="dob"
-        isRequired
-        isInvalid={!!errors.dob}
-        isReadOnly={updateMeMutation.isLoading}
-      >
-        <FormLabel>Date of Birth</FormLabel>
-        <DatePicker
-          selected={watchDob}
-          onChange={(date) => setValue('dob', date)}
-          dateFormat="dd/MM/yyyy"
-          showYearDropdown
-          showMonthDropdown
-          dropdownMode="select"
-        />
-        <FormErrorMessage>{errors.dob?.message}</FormErrorMessage>
-      </FormControl>
-      <FormControl
-        id="mobile"
-        isRequired
-        isInvalid={!!errors.mobile}
-        isReadOnly={updateMeMutation.isLoading}
-      >
-        <FormLabel>Mobile</FormLabel>
-        <Input
-          {...register('mobile')}
-          placeholder="Mobile"
+        <FormControl
+          id="name"
+          isRequired
+          isInvalid={!!errors.name}
+          isReadOnly={updateMeMutation.isLoading}
+          w="20rem"
+        >
+          <FormLabel>Name</FormLabel>
+          <Input
+            {...register('name')}
+            placeholder="Name"
+            borderRadius={'15px'}
+            textIndent={'0.5rem'}
+            h="1rem"
+            w="100%"
+          />
+          <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
+        </FormControl>
+        <FormControl
+          id="dob"
+          isRequired
+          isInvalid={!!errors.dob}
+          isReadOnly={updateMeMutation.isLoading}
+          w="20rem"
+        >
+          <FormLabel>Date of Birth</FormLabel>
+          <DatePicker
+            selected={watchDob}
+            onChange={(date) => setValue('dob', date)}
+            dateFormat="dd/MM/yyyy"
+            showYearDropdown
+            showMonthDropdown
+            dropdownMode="select"
+          />
+          <FormErrorMessage>{errors.dob?.message}</FormErrorMessage>
+        </FormControl>
+        <FormControl
+          id="mobile"
+          isRequired
+          isInvalid={!!errors.mobile}
+          isReadOnly={updateMeMutation.isLoading}
+          w="20rem"
+        >
+          <FormLabel>Mobile</FormLabel>
+          <Input
+            {...register('mobile')}
+            placeholder="Mobile"
+            borderRadius={'15px'}
+            textIndent={'0.5rem'}
+            h="1rem"
+            w="100%"
+          />
+          <FormErrorMessage>{errors.mobile?.message}</FormErrorMessage>
+        </FormControl>
+        <FormControl
+          id="email"
+          isRequired
+          isInvalid={!!errors.email}
+          isReadOnly={updateMeMutation.isLoading}
+          w="20rem"
+        >
+          <FormLabel>Email</FormLabel>
+          <Input
+            {...register('email')}
+            placeholder="Email"
+            borderRadius={'15px'}
+            textIndent={'0.5rem'}
+            h="1rem"
+            w="100%"
+          />
+          <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+        </FormControl>
+        <Button
+          onClick={async () => {
+            console.log(getValues())
+            await handleUpdate()
+          }}
+          isLoading={updateMeMutation.isLoading}
+          bgColor={'black'}
+          color="white"
           borderRadius={'15px'}
-        />
-        <FormErrorMessage>{errors.mobile?.message}</FormErrorMessage>
-      </FormControl>
-      <FormControl
-        id="email"
-        isRequired
-        isInvalid={!!errors.email}
-        isReadOnly={updateMeMutation.isLoading}
-      >
-        <FormLabel>Email</FormLabel>
-        <Input
-          {...register('email')}
-          placeholder="Email"
-          borderRadius={'15px'}
-        />
-        <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
-      </FormControl>
-      <Button
-        onClick={async () => {
-          console.log(getValues())
-          await handleUpdate()
-        }}
-        isLoading={updateMeMutation.isLoading}
-        bgColor={'black'}
-        color="white"
-        borderRadius={'15px'}
-        h={'2.75rem'}
-        w={'100px'}
-      >
-        Update
-      </Button>
-    </Stack>
+          h="2rem"
+          w="20rem"
+        >
+          Update
+        </Button>
+      </Stack>
+    </>
   )
 }
 export default Profile

4bd0c3364bd3eea599e06297d706eeb8f5eb6a18
Zhi <neozhixuan@gmail.com>
2024-08-25 15:09:03 +0200

    Indiv and overall donations styles

--- food-match/src/features/donations/Donations/DonationItemPage.tsx
+++ food-match/src/features/donations/Donations/DonationItemPage.tsx
@@ -1,5 +1,14 @@
-import { Box, Button, HStack, Stack, Text } from '@chakra-ui/react'
-import Image from 'next/image'
+import { AtSignIcon, CalendarIcon, InfoIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Button,
+  HStack,
+  Stack,
+  Text,
+  Image,
+  Flex,
+  VStack,
+} from '@chakra-ui/react'
 import { trpc } from '~/utils/trpc'
 
 export type DonationItemType =
@@ -22,47 +31,77 @@ export type DonationItemType =
   | undefined
 
 export const DonationItemPage = ({ item }: { item: DonationItemType }) => {
+  const formatISOString = (date: Date): string => {
+    return (
+      date.getUTCDate() + '/' + date.getUTCMonth() + '/' + date.getUTCFullYear()
+    )
+  }
+
   if (item) {
     const { data: donorData } = trpc.donor.getDonor.useQuery({
       donorId: item.donorId,
     })
     return (
-      <HStack p={20} w={'100%'} align={'start'} gap={20}>
-        <Box border={'1px solid black'} borderRadius={'15px'} w={'30%'}>
-          <Image
-            src={`${
-              item.imageUrls.length > 0
-                ? item.imageUrls[0]
-                : '/images/placeholder.png'
-            }`}
-            width={400}
-            height={500}
-            alt="Item Image"
-            style={{ borderRadius: '15px' }}
-          />
-        </Box>
-        <Stack>
-          <Text as="b">{item.name}</Text>
-          <Text>Description: {item.description}</Text>
-          <Text>Quantity: {item.quantity}</Text>
-
-          <Text>Donor: {donorData?.name}</Text>
-          <Text>Expiry: {item.expiry.toISOString()}</Text>
-          <Text>Created At: {item.createdAt.toISOString()}</Text>
-          {!item.beneficiaryId ? (
-            <Button
-              bgColor={'black'}
-              color={'white'}
-              h={'25px'}
-              borderRadius={'15px'}
-            >
-              I want to collect this item
-            </Button>
-          ) : (
-            <Text>
-              This item is being collected by user {item.beneficiaryId}.
+      <HStack
+        paddingInline={20}
+        marginBlock={'10vh'}
+        boxSizing="border-box"
+        h={'70vh'}
+        w={'100%'}
+        align={'start'}
+        gap={20}
+      >
+        <Image
+          src={`${
+            item.imageUrls.length > 0
+              ? item.imageUrls[0]
+              : '/images/placeholder.png'
+          }`}
+          alt="Item Image"
+          border={'1px solid black'}
+          borderRadius={'15px'}
+          w={'30%'}
+          h={'100%'}
+          objectFit={'cover'}
+        />
+        <Stack h={'100%'} justify={'space-between'}>
+          <Box>
+            <Text as="b">{item.name}</Text>
+            <Text color={'gray'}>
+              <CalendarIcon /> Expiry Date: {formatISOString(item.expiry)}
             </Text>
-          )}
+            <Text color={'gray'}>
+              <InfoIcon /> Quantity: {item.quantity}
+            </Text>
+
+            <Text color={'gray'}>
+              <AtSignIcon /> Donor: {donorData?.name}
+            </Text>
+          </Box>
+
+          <Box>
+            <Text>The donor says: {item.description}</Text>
+          </Box>
+          <Box>
+            {' '}
+            <Text color={'gray'}>
+              Created At: {formatISOString(item.createdAt)}
+            </Text>
+            {!item.beneficiaryId ? (
+              <Button
+                bgColor={'black'}
+                color={'white'}
+                h={'25px'}
+                borderRadius={'15px'}
+              >
+                I want to collect this item
+              </Button>
+            ) : (
+              <Text>
+                This item is being collected by user {item.beneficiaryId}.
+              </Text>
+            )}
+          </Box>
         </Stack>
       </HStack>
     )

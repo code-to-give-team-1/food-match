69fae97aa355c7e90006526de276878864661f88
Chang Xian Xiang <changxianxiang@gmail.com>
2024-08-25 20:36:49 +0800

    add seed file

---
+++ food-match/prisma/seed.ts
@@ -0,0 +1,118 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // create dummy users
+  const users = await prisma.user.createManyAndReturn({
+    data: [
+      {
+        name: 'Alice',
+        email: 'alice@example.com',
+        emailVerified: new Date(),
+      },
+      {
+        name: 'Bob',
+        email: 'bob@example.com',
+        emailVerified: new Date(),
+      },
+    ],
+  })
+
+  const alice = users.find((user) => user.name === 'Alice')
+  const bob = users.find((user) => user.name === 'Bob')
+
+  if (!alice || !bob) {
+    throw new Error('Could not find users')
+  }
+
+  console.log('Created users')
+
+  // create tags
+  const tags = await prisma.tag.createManyAndReturn({
+    data: [
+      { name: 'Vegetarian' },
+      { name: 'Vegan' },
+      { name: 'Gluten-free' },
+      { name: 'Dairy-free' },
+      { name: 'Halal' },
+      { name: 'Kosher' },
+    ],
+  })
+
+  console.log('Created tags')
+
+  const vegetarian = tags.find((tag) => tag.name === 'Vegetarian')
+  const vegan = tags.find((tag) => tag.name === 'Vegan')
+  const glutenFree = tags.find((tag) => tag.name === 'Gluten-free')
+  const dairyFree = tags.find((tag) => tag.name === 'Dairy-free')
+  const halal = tags.find((tag) => tag.name === 'Halal')
+  const kosher = tags.find((tag) => tag.name === 'Kosher')
+
+  if (!vegetarian || !vegan || !glutenFree || !dairyFree || !halal || !kosher) {
+    throw new Error('Could not find tags')
+  }
+
+  // create donations
+  await prisma.donation.createMany({
+    data: [
+      {
+        name: 'Vegan Pizza',
+        description: 'Vegan pizza with vegan cheese',
+        // one day before today
+        expiry: new Date(Date.now() - 24 * 60 * 60 * 1000),
+        quantity: '10',
+        tagsIds: [vegetarian.id, vegan.id],
+        donorId: alice.id,
+        beneficiaryId: bob.id,
+        passCode: '123456',
+      },
+      {
+        name: 'Gluten-free Bread',
+        description: 'Bread made from gluten-free flour',
+        // 1 week from today
+        expiry: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        quantity: '5',
+        tagsIds: [glutenFree.id],
+        donorId: alice.id,
+      },
+      {
+        name: 'Dairy-free Ice Cream',
+        description: 'Ice cream made without dairy',
+        // 1 week from today
+        expiry: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        quantity: '8',
+        tagsIds: [dairyFree.id],
+        donorId: bob.id,
+      },
+      {
+        name: 'Halal Chicken',
+        description: 'Halal chicken meat',
+        // 1 week from today
+        expiry: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        quantity: '15',
+        tagsIds: [halal.id],
+        donorId: bob.id,
+      },
+      {
+        name: 'Kosher Fish',
+        description: 'Kosher fish',
+        // 1 week from today
+        expiry: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        quantity: '12',
+        tagsIds: [kosher.id],
+        donorId: bob.id,
+      },
+    ],
+  })
+
+  console.log('Created donations')
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })

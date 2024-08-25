32ead5a164ce17b420180c35018e786a2f27c294
Chang Xian Xiang <changxianxiang@gmail.com>
2024-08-26 06:14:02 +0800

    unguard home

--- food-match/src/pages/home.tsx
+++ food-match/src/pages/home.tsx
@@ -1,18 +1,8 @@
 import { Box } from '@chakra-ui/react'
-import { useRouter } from 'next/router'
 import { Navbar } from '~/features/common/components'
-import { useMe } from '~/features/me/api'
 import { DonationsPage } from '~/features/donations'
 
 const Home = () => {
-  // Login status
-  const { me } = useMe()
-  const router = useRouter()
-
-  if (!me.isProfileComplete) {
-    void router.push('/profile')
-  }
-  // Components
   return (
     <Box m={0} w={'100%'}>
       <Navbar />
